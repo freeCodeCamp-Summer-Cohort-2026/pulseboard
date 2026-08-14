442ee0f1b653b79a5348c25371b0fa51f2be1401
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-08-14 02:43:12 +0200

    tests: add tests for saving submissions in LocalStorage
    This adds tests for how the UpdateForm element behaves when receiving Network Error while sending a submission.
    Tests include recognizing Network Errors on Firefox and Chrome web browsers.

--- web/app/components/__tests__/UpdateForm.test.js
+++ web/app/components/__tests__/UpdateForm.test.js
@@ -170,3 +170,76 @@ describe("CountCharacters", () => {
     expect(screen.getByText("12/1000")).toBeInTheDocument();
   });
 });
+
+describe("UpdateForm saving submissions", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    localStorage.clear();
+  });
+
+  it("stores the form submission in LocalStorage on network error (Firefox)", async () => {
+    let rejectRequest = new TypeError(
+      "NetworkError when attempting to fetch resource.",
+    );
+
+    createUpdate.mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        reject(rejectRequest);
+      });
+    });
+
+    const onPosted = jest.fn();
+
+    render(<UpdateForm auth={auth} onPosted={onPosted} />);
+
+    const textarea = screen.getByPlaceholderText(/what's your status today/i);
+    const select = screen.getByRole("combobox");
+    const button = screen.getByRole("button", { name: /post update/i });
+
+    fireEvent.change(textarea, {
+      target: { value: "test" },
+    });
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(screen.getByText("Failed to connect.")).toBeInTheDocument();
+    });
+
+    expect(localStorage.getItem("queuedMessages")).toMatch(
+      /\[{\"id\":\"test\d+\",\"text\":\"test\",\"status\":\"on-track\"}\]/,
+    );
+  });
+
+  it("stores the form submission in LocalStorage on network error (Chrome)", async () => {
+    let rejectRequest = new TypeError("Failed to fetch");
+
+    createUpdate.mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        reject(rejectRequest);
+      });
+    });
+
+    const onPosted = jest.fn();
+
+    render(<UpdateForm auth={auth} onPosted={onPosted} />);
+
+    const textarea = screen.getByPlaceholderText(/what's your status today/i);
+    const select = screen.getByRole("combobox");
+    const button = screen.getByRole("button", { name: /post update/i });
+
+    fireEvent.change(textarea, {
+      target: { value: "test" },
+    });
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(screen.getByText("Failed to connect.")).toBeInTheDocument();
+    });
+
+    expect(localStorage.getItem("queuedMessages")).toMatch(
+      /\[{\"id\":\"test\d+\",\"text\":\"test\",\"status\":\"on-track\"}\]/,
+    );
+  });
+});
